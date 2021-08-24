4c9aa46d20ef985cf970e2ec0ad05f203a9e0e2b
maisern <71262624+maisern@users.noreply.github.com>
2021-08-24 09:32:37 +0200

    Add files via upload
    la til filene som av en eller annen grunn ikke kom i stad.

---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[]args){
+
+        System.out.println("Hello World");
+
+        System.out.println("teller til 10");
+        for (int i=1; i<11;i++){
+            System.out.println(i);
+        }
+    }
+}
