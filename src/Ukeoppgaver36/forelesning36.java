16782d9a94b83efbe5daed74e90e67189448e537
Maisern <mai8lise@hotmail.com>
2022-09-08 09:19:16 +0200

    lagt til uke 36

---
+++ src/Ukeoppgaver36/forelesning36.java
@@ -0,0 +1,17 @@
+package Ukeoppgaver36;
+
+public class forelesning36 {
+
+
+    public static void quicksort(int[] a, int fra, int til) {
+/*
+        if (fra>=til)return;
+            int p = byttPiviotBakerst(a);
+        int six = partisjonering(a,p);
+        byttPiviotBakerst(a,six);
+
+
+ */
+    }
+
+}
